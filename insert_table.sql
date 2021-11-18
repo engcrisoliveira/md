f57f29938973479cb7d3b055ba2055f49090a8b1
engcrisoliveira <crisoliveira.clr@gmail.com>
2021-11-18 13:49:49 -0300

    Create insert_table.sql

---
+++ insert_table.sql
@@ -0,0 +1,31 @@
+insert into recursos (id_recurso, recurso) values 
+	(1, 'comunicação'),
+    (2, 'informática');
+    
+insert into interprete (id_interprete, tipo_interprete) values 
+	(1, 'tradutor de libras'),
+    (2, 'guia-intérprete');
+    
+insert into material (id_material, tipo_do_material) values
+	(1, 'material digital'),
+    (2, 'material ampliado'),
+    (3, 'material tátil'),
+    (4, 'material impresso'),
+    (5, 'material em áudio'),
+    (6, 'material em braille'),
+    (7, 'material em libras');
+    
+insert into deficiencias (id_deficiencia, tipo_deficiencia) values
+	(1, 'deficiência auditiva'),
+    (2, 'deficiência auditiva'),
+    (3, 'deficiência auditiva'),
+    (4, 'deficiência auditiva'),
+    (5, 'surdez'),
+    (6, 'surdocegueira'),
+    (7, 'baixa visão'),
+    (8, 'cegueira'),
+    (9, 'superdotação'),
+    (10, 'autismo'),
+    (11, 'síndrome de asperger'),
+    (12, 'síndrome de rett'),
+    (13, 'transtorno desintegrativo');
